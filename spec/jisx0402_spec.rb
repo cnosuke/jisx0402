449396427b58a7726ad6f52ab53901b33f2f4cb6
cnosuke <shinnosuke@gmail.com>
2016-02-19 18:37:29 +0900

    write specs

--- spec/jisx0402_spec.rb
+++ spec/jisx0402_spec.rb
@@ -5,7 +5,45 @@ describe Jisx0402 do
     expect(Jisx0402::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  let(:shibuya_code) { "131130" }
+
+  it 'can find 東京都渋谷区' do
+    expect(Jisx0402.search('東京都渋谷区').code).to eq(shibuya_code)
+  end
+
+  it 'can find 渋谷区' do
+    expect(Jisx0402.search('渋谷区').code).to eq(shibuya_code)
+  end
+
+  it 'can find しぶや' do
+    expect(Jisx0402.search('しぶや').code).to eq(shibuya_code)
+  end
+
+  it 'has code without checkdigit' do
+    expect(Jisx0402.search('しぶや').code_without_checkdigit).to eq(shibuya_code[0..-2])
+  end
+
+  it 'has zipcodes including 渋谷区' do
+    expect(Jisx0402.search('渋谷区').zipcodes).to eq(
+      [
+        "1500000", "1510064", "1500032", "1500042", "1500013", "1506090",
+        "1506001", "1506002", "1506003", "1506004", "1506005", "1506006",
+        "1506007", "1506008", "1506009", "1506010", "1506011", "1506012",
+        "1506013", "1506014", "1506015", "1506016", "1506017", "1506018",
+        "1506019", "1506020", "1506021", "1506022", "1506023", "1506024",
+        "1506025", "1506026", "1506027", "1506028", "1506029", "1506030",
+        "1506031", "1506032", "1506033", "1506034", "1506035", "1506036",
+        "1506037", "1506038", "1506039", "1500021", "1500022", "1510065",
+        "1500047", "1500031", "1510073", "1500033", "1500002", "1500046",
+        "1500001", "1500045", "1500041", "1510051", "1500034", "1500043",
+        "1510063", "1500036", "1510066", "1510072", "1500035", "1510061",
+        "1500011", "1500012", "1510071", "1500044", "1510062", "1510053",
+        "1510052",
+      ]
+    )
+  end
+
+  it 'has zipcodes including 東京都' do
+    expect(Jisx0402.search('東京都').zipcodes.size).to eq(3806)
   end
 end
